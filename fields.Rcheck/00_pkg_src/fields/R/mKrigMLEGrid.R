f415d661847dcbda3e0131820b1bcffe4e66b5fe
Douglas Nychka <nychka@mines.edu>
2019-04-07 12:17:59 -0600

    beta 9.7

---
+++ fields.Rcheck/00_pkg_src/fields/R/mKrigMLEGrid.R
@@ -0,0 +1,149 @@
+# fields  is a package for analysis of spatial data written for
+# the R software environment .
+# Copyright (C) 2018
+# University Corporation for Atmospheric Research (UCAR)
+# Contact: Douglas Nychka, nychka@ucar.edu,
+# National Center for Atmospheric Research, PO Box 3000, Boulder, CO 80307-3000
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with the R software environment if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+# or see http://www.r-project.org/Licenses/GPL-2
+
+mKrigMLEGrid <- function(x, y, weights = rep(1, nrow(x)), Z = NULL,
+                       mKrig.args = NULL,
+                          cov.fun = "stationary.cov", 
+                         cov.args = NULL,
+                           na.rm = TRUE, 
+                         par.grid = NULL, 
+                           lambda = NULL, 
+                   lambda.profile = TRUE, 
+               relative.tolerance = 1e-04,
+                             REML = FALSE,
+                          verbose = FALSE) {
+  if( na.rm){
+    obj<- mKrigCheckXY(x, y, weights, Z, na.rm)
+    x<- obj$x
+    y<- obj$y
+    weights<- obj$weights
+    Z<- obj$Z
+  }
+  #check which optimization options the covariance function supports
+  #precompute distance matrix if possible so it only needs to be computed once
+  supportsDistMat = supportsArg(cov.fun, "distMat")
+  #precompute distance matrix if possible so it only needs to be computed once
+  if(supportsDistMat) {
+    #Get distance function and arguments if available
+    #If user left all distance settings NULL, use rdist with compact option.
+    #Use rdist function by default in general.
+    #
+    if(is.null(cov.args$Distance)) {
+      cov.args$Distance  <-  "rdist"
+      cov.args$Dist.args <- list(compact=TRUE)
+    }
+    cov.args$distMat<-do.call(cov.args$Distance, c( list(x), cov.args$Dist.args) )
+    cov.args$onlyUpper<- TRUE
+    }
+
+  lnProfileLike.max <- -1e+20
+# find NG --  number of parameters to try
+  par.grid <- data.frame(par.grid)
+  if (nrow(par.grid) == 0) {
+    NG<- ifelse(is.null(lambda), 1, length( lambda)) 
+  }
+  else {
+    NG <- nrow(par.grid)
+  }
+  lambda.best <- NA
+  # default for lambda is 1.0 for first value and exp(llambda.opt) for subsequent ones
+  # this is controlled by NAs for lambda starting values.
+  if (is.null(lambda)) {
+    lambda <- rep(NA, NG)
+  }
+  # output matrix to summarize results
+  summary <- matrix(NA, nrow = NG, ncol = 8)
+  
+  # default starting value for lambda is .5 or log lambda is 0
+  lambda.opt <- .5
+  optim.counts <- c(NA, NA)
+  lnLike.eval <- list()
+  # Define the objective function as a tricksy call to mKrig
+  # if Y is a matrix of replicated data sets use the log likelihood for the complete data sets
+  #
+  # begin loop over covariance arguments
+  lnLike.eval<- list()
+  for (k in 1:NG) {
+    lambda.start <- ifelse(is.na(lambda[k]), lambda.opt, (lambda[k]))
+    # list of covariance arguments from par.grid with right names (some R arcania!)
+    # note that this only works because 1) temp.fn will search in this frame for this object
+    # par.grid has been coerced to a data frame so one has a concept of a row subscript.
+    cov.args.temp <- as.list(par.grid[k, ])
+    names(cov.args.temp) <- names(par.grid)
+    currentCov.args<- c(cov.args.temp, cov.args) 
+    # optimize over lambda if lambda.profile is TRUE
+    optim.args = list(method = "BFGS", 
+                      control = list(fnscale = -1, parscale = c(0.5), 
+                                     ndeps = c(0.05)))
+    if (lambda.profile) {
+      # set up matrix to store evaluations from within optim
+    MLEfit0 <- mKrigMLEJoint(x, y, weights=weights, Z=Z, 
+                             lambda.start = lambda.start, 
+                         cov.params.start = NULL, 
+                                  cov.fun = cov.fun,
+                               optim.args = optim.args,
+                                 cov.args = currentCov.args,
+                                    na.rm = na.rm,
+                               mKrig.args = mKrig.args,
+                                     REML = REML,
+                                  verbose = verbose)
+    lnLike.eval<- c( lnLike.eval, list(MLEfit0$lnLike.eval))
+    lambda.opt<- MLEfit0$pars.MLE[1]
+    }
+    else {
+      # no refinement for lambda so just save the the 'start' value as final one.
+      lambda.opt <- lambda.start
+    }
+    
+# final fit at optimal value 
+#    (or starting value if not refinement/maximization for lambda)
+    obj <- do.call("mKrig", c(
+      list(x = x, y = y, weights = weights, Z = Z, na.rm = na.rm),
+                                    mKrig.args,
+       list(lambda=lambda.opt),
+       list( cov.fun= cov.fun, cov.args = currentCov.args)
+      )
+      )
+    nameCriterion<- ifelse( !REML,
+                            "lnProfileLike.FULL",
+                            "lnProfileREML.FULL" )
+    if (obj[[nameCriterion]] > lnProfileLike.max) {
+      lnProfileLike.max <- obj$lnProfileLike.FULL
+      cov.args.MLE <- cov.args.temp
+      lambda.best <- lambda.opt
+    }
+    
+# save results of the kth covariance model evaluation
+    summary[k, 1:8] <- c(obj$eff.df, obj[[nameCriterion]], 
+                         obj$GCV, obj$sigma.MLE.FULL, obj$rho.MLE.FULL, lambda.opt, 
+                         optim.counts)
+    dimnames(summary) <- list(NULL, c("EffDf",nameCriterion , 
+                                      "GCV", "sigma.MLE", "rho.MLE", "lambda.MLE", "counts eval", 
+                                      "counts grad"))
+    if (verbose) {
+      cat("Summary: ", k, summary[k, 1:8], fill = TRUE)
+    }
+  }
+  return(list(summary = summary, par.grid = par.grid, cov.args.MLE = cov.args.MLE, 
+              lambda.best = lambda.best, lambda.MLE = lambda.best, 
+              call = match.call(), lnLike.eval = lnLike.eval)
+         )
+}
---
+++ fields.Rcheck/00_pkg_src/fields/tests/cov.test.R
@@ -0,0 +1,157 @@
+ # fields is a package for analysis of spatial data written for
+  # the R software environment .
+  # Copyright (C) 2018
+  # University Corporation for Atmospheric Research (UCAR)
+  # Contact: Douglas Nychka, nychka@ucar.edu,
+  # National Center for Atmospheric Research,
+  # PO Box 3000, Boulder, CO 80307-3000
+  #
+  # This program is free software; you can redistribute it and/or modify
+  # it under the terms of the GNU General Public License as published by
+  # the Free Software Foundation; either version 2 of the License, or
+  # (at your option) any later version.
+  # This program is distributed in the hope that it will be useful,
+  # but WITHOUT ANY WARRANTY; without even the implied warranty of
+  # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  # GNU General Public License for more details.
+
+
+suppressMessages(library(fields))
+options( echo=FALSE)
+test.for.zero.flag<- 1
+data(ozone2)
+y<- ozone2$y[16,]
+x<- ozone2$lon.lat
+#
+# Omit the NAs
+good<- !is.na( y)
+x<- x[good,]
+y<- y[good]
+x1<- x[1:5,]
+x2<- x[6:11,]
+
+look<- exp(-1*rdist(x1,x2)/4)
+look2<- stationary.cov( x1,x2, theta=4)
+look3<- Exp.cov( x1, x2, theta=4.0)
+test.for.zero( look, look2)
+test.for.zero( look, look3)
+
+set.seed(122)
+C<-  rnorm( nrow(x2))
+look<- exp(-1*rdist(x1,x2)/4)%*%C
+look2<- stationary.cov( x1,x2, theta=4, C=C)
+look3<- Exp.cov( x1, x2, theta=4.0, C=C)
+test.for.zero( look, look2)
+test.for.zero( look, look3)
+
+#### check tranformation of coordinates
+V<- matrix( c(2,1,0,4), 2,2)
+Vi<- solve( V)
+u1<- t(Vi%*% t(x1))
+u2<- t(Vi%*% t(x2))
+
+look<- exp(-1*rdist(u1,u2))
+look2<- stationary.cov( x1,x2, V= V)
+test.for.zero( look, look2)
+
+look<- Wendland(rdist(u1,u2), k=3, dimension=2)
+look2<- stationary.cov( x1,x2, V= V, Covariance = "Wendland",
+                       k=3, dimension=2)
+test.for.zero( look, look2)
+
+### check tapering of covariances
+x1<- x[1:5,]
+x2<- x[2:6,]
+V<- matrix( c(2,1,0,4), 2,2)
+Vi<- solve( V)
+
+u1<- x1
+u2<- x2
+
+look1a<- exp(-1*rdist(u1,u2))
+look1b<-  Wendland(rdist(u1,u2),
+                                      k=3, dimension=2, theta= 1)
+look1<- look1a*look1b
+look2<- stationary.taper.cov( x1,x2, theta=1,
+               Taper.args=list( theta=1,k=3, dimension=2), verbose=FALSE)
+test.for.zero( look1, as.matrix(look2))
+
+
+u1<- t(Vi%*% t(x1))
+u2<- t(Vi%*% t(x2))
+
+
+look1a<- exp(-1*rdist(u1,u2))
+look1b<-  Wendland(rdist(u1,u2),
+                                      k=3, dimension=2, theta= 1.5)
+look1<- look1a*look1b
+look2<- stationary.taper.cov( x1,x2,V=V,
+               Taper.args=list( theta=1.5,k=3, dimension=2), verbose=FALSE)
+test.for.zero( look1, as.matrix(look2))
+
+
+u1<- t(Vi%*% t(x1))
+u2<- t(Vi%*% t(x2))
+
+
+look1a<- Matern(rdist(u1,u2), smoothness=1.5)
+look1b<-  Wendland(rdist(u1,u2),
+                                      k=3, dimension=2, theta= 1.5)
+look1<- look1a*look1b
+look2<- stationary.taper.cov( x1,x2,V=V,Covariance=Matern, smoothness=1.5,
+               Taper.args=list( theta=1.5,k=3, dimension=2), verbose=FALSE)
+test.for.zero( look1, as.matrix(look2))
+
+
+# some tests of great circle distance
+
+
+stationary.taper.cov( x[1:3,],x[1:10,] , theta=200, Taper.args= 
+       list(k=2,theta=300, dimension=2),
+       Dist.args=list( method="greatcircle") )-> temp
+
+# temp is now a tapered 3X10 cross covariance matrix in sparse format. 
+# should be identical to
+# the direct matrix product
+
+temp2<- Exponential( rdist.earth(x[1:3,],x[1:10,]), range=200) * 
+           Wendland(rdist.earth(x[1:3,],x[1:10,]), theta= 300, k=2, dimension=2)
+
+test.for.zero(  as.matrix(temp), temp2, tol=1e-6, tag="taper with great circle")
+
+# example of calling the taper version directly 
+# Note that default covariance is exponential and default taper is 
+# Wendland (k=2).
+
+stationary.taper.cov( x[1:3,],x[1:10,] , theta=1.5, Taper.args= 
+      list(k=2,theta=2.0, dimension=2) )-> temp
+# temp is now a tapered 5X10 cross covariance matrix in sparse format. 
+# should be identical to
+# the direct matrix product
+
+temp2<- Exp.cov( x[1:3,],x[1:10,], theta=1.5) * 
+           Wendland(rdist(x[1:3,],x[1:10,]),
+                      theta= 2.0, k=2, dimension=2)
+
+test.for.zero(  as.matrix(temp), temp2, tag= "high level test of taper cov")
+
+stationary.taper.cov( x[1:3,],x[1:10,] , range=1.5,
+        Taper.args= list(k=2,theta=2.0,
+                       dimension=2) )-> temp
+
+test.for.zero(  as.matrix(temp), temp2, tag= "high level test of taper cov")
+
+cat("end tests of V argument in covariances", fill=TRUE)
+
+
+
+
+
+
+
+
+
+
+
+
+

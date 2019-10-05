5fb024e68502d01c0e175f2d9f791efd80bf7b8e
Douglas Nychka <nychka@mines.edu>
2019-10-05 08:35:13 -0600

    Version 9.9 for testing with Florian

--- fields/R/color.scale.R
+++ fields/R/color.scale.R
@@ -43,8 +43,9 @@ color.scale <- function(z, col = tim.colors(256),
 # legend.     
     attr( colorMap,"zlim")<- zlim
     attr(colorMap,"col")<- col
-    colorMapInfo<- list( col=col, zlim=zlim)
-    assign( ".colorMapInfo", colorMapInfo, pos=1)
+    # potential hooks for colorBar functions
+    #colorMapInfo<- list( col=col, zlim=zlim)
+    #assign( ".colorMapInfo", colorMapInfo, pos=1)
     return( colorMap)
 }
 
